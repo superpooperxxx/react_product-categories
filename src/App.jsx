d959bce980eb1770280836678c9820fc6087784c
Misha Grinko <glukym@gmail.com>
2024-01-04 11:03:22 +0200

    fix deps for JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import productsFromServer from './api/products';
-// import categoriesFromServer from './api/categories';
+import usersFromServer from './api/users';
+import productsFromServer from './api/products';
+import categoriesFromServer from './api/categories';
 
-export const App: React.FC = () => (
+export const App = () => (
   <div className="container">
     <div className="ui card">
       <div className="ui content">
         <div className="ui description">
           <p>Grocery - (Anna)</p>
-          
+
           <ul className="ui list">
             <li>Bread</li>
             <li>Eggs</li>
